4be7721aa5db3c091ba9075db00f275444ac258b
JoseManzi <cote.manzi.35@gmail.com>
2024-08-07 19:51:58 -0300

    Cambios Varios

--- wwwroot/js/layout.js
+++ wwwroot/js/layout.js
@@ -1,25 +1,3 @@
-// // 
-// // Scripts
-// // 
-
-// window.addEventListener('DOMContentLoaded', event => {
-
-//     // Toggle the side navigation
-//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
-//     if (sidebarToggle) {
-//         // Uncomment Below to persist sidebar toggle between refreshes
-//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-//         //     document.body.classList.toggle('sb-sidenav-toggled');
-//         // }
-//         sidebarToggle.addEventListener('click', event => {
-//             event.preventDefault();
-//             document.body.classList.toggle('sb-sidenav-toggled');
-//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-//         });
-//     }
-
-// });
-
 window.addEventListener('DOMContentLoaded', event => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
@@ -32,16 +10,6 @@ window.addEventListener('DOMContentLoaded', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-
-            // Obtén una referencia al wrapper del sidebar
-            const sidebarWrapper = document.getElementById('sidebar-wrapper');
-
-            // Establecer un ancho mínimo para el sidebar cuando se cierra
-            if (document.body.classList.contains('sb-sidenav-toggled')) {
-                sidebarWrapper.style.width = '300px'; // Ajusta este valor según tus necesidades
-            } else {
-                sidebarWrapper.style.width = ''; // Vuelve al ancho original
-            }
         });
     }
 });
